03168ecf27e7f8d040f8f58790613f6f55d3df69
Nuth69 <pravevinuth888@gmail.com>
2026-06-26 15:50:56 +0700

    fix

--- lib/core/constants/api_config.dart
+++ lib/core/constants/api_config.dart
@@ -3,8 +3,8 @@ import 'package:flutter/foundation.dart';
 abstract class ApiConfig {
   static String get baseUrl {
     if (kIsWeb) {
-      return 'http://127.0.0.1:3000';
+      return 'http://127.0.0.1:3001';
     }
-    return 'http://127.0.0.1:3000';
+    return 'http://127.0.0.1:3001';
   }
 }
